2cf3dbcf5bb39d3adbaeb0e82b576f10afed8ca8
Rob Thijssen <rthijssen@gmail.com>
2017-05-17 12:00:12 +0300

    abstracted mailbox storage

--- mailbox/src/main.rs
+++ mailbox/src/main.rs
@@ -7,27 +7,58 @@ use std::thread;
 use std::sync::{Arc, Mutex};
 
 fn main() {
-  fn handle_client(mut stream: TcpStream, messages: Arc<Mutex<VecDeque<String>>>) {
+  //fn handle_request<S: Storage<String>>(stream: &mut TcpStream, storage: &S) {
+  fn handle_request<S: Storage<String>>(stream: &mut TcpStream, storage: Arc<S>) {
     let mut buffer = String::new();
     stream.read_to_string(&mut buffer);
     let result = parse(&buffer).unwrap();
     match result {
-      Command::Publish(message) => messages.lock().unwrap().push_back(message),
+      Command::Publish(message) => storage.put(message),
       Command::Retrieve => {
-        match stream.write(messages.lock().unwrap().pop_back().unwrap().as_bytes()) {
-          Ok(_) => println!("Response sent"),
+        match stream.write(storage.get().unwrap().as_bytes()) {
+          Ok(_) => println!("Response sent."),
           Err(e) => println!("Failed sending response: {}", e),
         }
-      }
+      },
     }
   }
-  let messages = Arc::new(Mutex::new(VecDeque::with_capacity(10)));
+  let arced_mailbox: Arc<SyncedMailbox<String>> = Arc::new(SyncedMailbox::new());
   let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
   for stream in listener.incoming() {
-    let storage = messages.clone();
+    let cloned_arc = arced_mailbox.clone();
     thread::spawn(move || match stream {
-      Ok(stream) => handle_client(stream, storage),
+      Ok(mut stream) => {
+        //handle_request(&mut stream, cloned_arc.as_ref());
+        handle_request(&mut stream, cloned_arc);
+        println!("Request received.")
+      },
       Err(e) => println!("Failed receiving request: {}", e),
     });
+  }
+}
+
+struct SyncedMailbox<T> {
+  all_the_mail: Mutex<VecDeque<T>>,
+}
+
+impl<T> SyncedMailbox<T> {
+  fn new () -> SyncedMailbox<T> {
+    SyncedMailbox {
+      all_the_mail: Mutex::new(VecDeque::new())
+    }
+  }
+}
+
+trait Storage<T> {
+  fn put(&self, item: T) -> ();
+  fn get(&self) -> Option<T>;
+}
+
+impl<T> Storage<T> for SyncedMailbox<T> {
+  fn put(&self, item: T) {
+    self.all_the_mail.lock().unwrap().push_back(item);
+  }
+  fn get(&self) -> Option<T> {
+    self.all_the_mail.lock().unwrap().pop_back()
   }
 }
